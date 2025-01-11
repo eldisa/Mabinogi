df1b480705340556b1197aee9dc1b0c3a7d0dc01
eldisa <maplestory54007@gmail.com>
2025-01-11 13:18:42 +0800

    fix: make hex code be Uppercase

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,5 +2,5 @@ export function toHex(value: number) {
     if (value < 0 || value > 255) {
         throw new RangeError("Input must be between 0 and 255.");
     }
-    return value.toString(16).padStart(2, "0");
+    return value.toString(16).toUpperCase().padStart(2, "0");
 }
