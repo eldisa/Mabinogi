1a2cf0062f6c1b21cee54bf931a4e9d54a2d28d1
eldisa <maplestory54007@gmail.com>
2025-01-11 13:12:10 +0800

    feat: update text

--- src/components/ModeSelect.tsx
+++ src/components/ModeSelect.tsx
@@ -5,11 +5,12 @@ const modeOptions = [
     // todo: remove 1 &2 , merge to single select
     { label: "固定顏色 1", value: 1 },
     { label: "固定顏色 2", value: 2 },
-    { label: "規則", value: 4 },
-    { label: "不規則", value: 8 },
+    { label: "正向漸進", value: 4 },
+    { label: "逆向漸進", value: 5 },
+    { label: "隨機漸進", value: 8 },
     { label: "往返", value: 6 },
-    { label: "切換(不同步)", value: 7 },
-    { label: "切換(同步)", value: 9 },
+    { label: "雙色隨機切換(不同步)", value: 7 },
+    { label: "雙色隨機切換(同步)", value: 9 },
 ] as const;
 
 interface ModeSelectProps {
