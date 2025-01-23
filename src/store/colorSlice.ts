f1010f912683b6adfd1058ab44ceda8413912ce4
eldisa <maplestory54007@gmail.com>
2025-01-23 23:37:52 +0800

    feat: add save utility at redux

--- src/store/colorSlice.ts
+++ src/store/colorSlice.ts
@@ -1,14 +1,21 @@
-// filepath: /c:/Users/User/WebstormProjects/mabinogi/src/features/colorSlice.ts
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface ColorState {
     colorArray: string[];
     startIndex: number;
+    savedColorCode: {
+        single: string[];
+        shiny: string[];
+    };
 }
 
 const initialState: ColorState = {
     colorArray: [],
     startIndex: 0,
+    savedColorCode: {
+        single: [],
+        shiny: [],
+    },
 };
 
 const colorSlice = createSlice({
@@ -21,8 +28,19 @@ const colorSlice = createSlice({
         setStartIndex: (state, action: PayloadAction<number>) => {
             state.startIndex = action.payload;
         },
+        setSingleColorCode: (state, action: PayloadAction<string[]>) => {
+            state.savedColorCode.single = action.payload;
+        },
+        setShinyColorCode: (state, action: PayloadAction<string[]>) => {
+            state.savedColorCode.shiny = action.payload;
+        },
     },
 });
 
-export const { setColorArray, setStartIndex } = colorSlice.actions;
+export const {
+    setColorArray,
+    setStartIndex,
+    setSingleColorCode,
+    setShinyColorCode,
+} = colorSlice.actions;
 export default colorSlice.reducer;
